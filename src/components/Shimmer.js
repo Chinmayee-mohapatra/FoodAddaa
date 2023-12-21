41ef14dc0a00844f875ed5d5518cda06f6df7f06
Chinmayee-mohapatra <94803850+Chinmayee-mohapatra@users.noreply.github.com>
2023-12-21 11:35:51 +0530

    shimmer updated

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -4,7 +4,7 @@ import ShimmerCard from "./ShimmerCard";
 const Shimmer = () => {
   return (
     <div className="flex flex-wrap gap-2 m-4 p-4">
-      {Array.from({ length: 18 }, (_, i) => (
+      {Array.from({ length: 18 }, (x, i) => (
         <ShimmerCard />
       ))}
     </div>
